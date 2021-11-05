9d363125ad12fa68c5827b024f781dfee523eed9
lordbaduk <>
2021-11-05 15:46:55 +0100

    add more comments

--- asmtutor/01_hello_world.asm
+++ asmtutor/01_hello_world.asm
@@ -6,14 +6,18 @@
 ;   $ ld -lSystem 01_hello_world.o
 
 
-; LABELS    INSTRUCTIONS    OPERANDS           COMMENTS
+; LABELS    INSTRUCTIONS    OPERANDS            LINE COMMENTS
 
 ; --------------------------------------------------------------- global directive
-            global          _main
+            global          _main               ; the macos linker looks for label
+                                                ; '_main' as the main entry point.
 
 ; ------------------------------------------------------------------ section .text
             section         .text
 _main:
+            ; note in the following code how the registers for the system call
+            ; arguments are different for x86-32 and x86-64 (see System V ABI
+            ; conventions).
             mov             rax, 0x02000004     ; system call for write
             mov             rdi, 1              ; file handle 1 is stdout
             mov             rsi, message        ; address of string to output
@@ -21,7 +25,8 @@ _main:
             syscall                             ; invoke operating system to
                                                 ; do the write
 
-            ; will segfault here
+            ; a segmentation fault will occur here because the program is not stopped
+            ; and the program counter advances into an invalid memory region.
 
 ; -------------------------------------------------------------------------- .text
             section         .data
