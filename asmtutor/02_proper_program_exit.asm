81a121a0d5ea5f287f74ac07473be454b18c8b26
lordbaduk <>
2021-11-05 18:44:31 +0100

    improve comments

--- asmtutor/02_proper_program_exit.asm
+++ asmtutor/02_proper_program_exit.asm
@@ -14,14 +14,14 @@
 ; ------------------------------------------------------------------ section .text
             section         .text
 _main:
-            mov             rax, 0x02000004     ; system call for write
-            mov             rdi, 1              ; file handle 1 is stdout
-            mov             rsi, message        ; address of string to output
-            mov             rdx, 13             ; number of bytes
+            mov             rax, 0x02000004     ; opcode: system call for write
+            mov             rdi, 1              ; arg1: file handle 1 is stdout
+            mov             rsi, message        ; arg2: address of string to output
+            mov             rdx, 13             ; arg3: number of bytes
             syscall                             ; invoke operating system to
                                                 ; do the write
-            mov             rax, 0x02000001     ; system call for exit
-            xor             rdi, rdi            ; exit code 0
+            mov             rax, 0x02000001     ; opcode: system call for exit
+            xor             rdi, rdi            ; arg1: exit code 0
             syscall                             ; invoke operating system to exit
 
 ; -------------------------------------------------------------------------- .text
