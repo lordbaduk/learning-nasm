0b2bb4d4108e6f8b6ec0902ec12ea5c42ef5bf1e
lordbaduk <>
2021-11-25 23:04:53 +0100

    add lesson 12 code

---
+++ asmtutor/12_calculator_addition.asm
@@ -0,0 +1,30 @@
+; ------------------------------------------------------------------------------
+; https://asmtutor.com/#lesson12 "Calculator - addition"
+;
+; assemble and link as follows:
+;   $ nasm -f macho64 12_calculator_addition.asm
+;   $ ld -lSystem 12_calculator_addition.o
+
+%include    'functions.asm'
+
+    global      _main
+
+; ------------------------------------------------------------------------- code
+
+    section     .text
+
+_main:
+
+    mov         rax, 0x10
+    add         rax, 0x10
+    mov         rdi, rax
+
+    call        iprint
+    cmp         rax, 0x0
+    jne         done_errors
+
+done:
+    call        exit_no_errors
+
+done_errors:
+    call        exit_with_error
